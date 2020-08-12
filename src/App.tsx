199f3a9c5814f2f4644548ca39feed2d8d9a5d76
Kestutis1993 <kestutis.pajaujis93@gmail.com>
2020-08-12 22:24:20 +0300

    configured webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {
+  name: string;
 }
+
+class App extends React.Component<IProps, {}> {
+  render () {
+    return (
+      <div>
+        <h1>Welcome to React + Typescript</h1>
+        <p>The color of this page is: {this.props.name}</p>
+      </div>
+    );
+  }
+}
+
 
 export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,39 @@
+const path = require('path'), HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    resolve: {
+        extensions: [ '.ts', '.tsx', '.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+        ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.ejs',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true
+            },
+        }),
+    ],
+};
